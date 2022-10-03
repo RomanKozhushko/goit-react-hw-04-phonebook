93bb4953344d4ca9a983caf7a740b06090a497a1
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-10-03 10:24:21 +0100

    Update InputFormAddContacts.jsx

--- src/components/InputFormAddContacts/InputFormAddContacts.jsx
+++ src/components/InputFormAddContacts/InputFormAddContacts.jsx
@@ -49,7 +49,6 @@ export const InputForm = ({ onSubmit }) => {
                     pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                     title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                          required
-                                           // onChange={this.onInput}
                     /></label>
                     <button type="submit">Add contact</button>
                 </Form>
